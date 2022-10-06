285b2b4f685fc85d426307b5b63581040863ecff
Xkriso <thekriso@wp.pl>
2022-10-06 23:02:21 +0200

    New components added

---
+++ src/data/users.js
@@ -0,0 +1,23 @@
+export const users = [
+    {
+        name: 'Krzysztof Olejniczak',
+        attendance: '39%',
+        average:2.3,
+    },
+    {
+        name: 'Szymon Janiak',
+        attendance: '64%',
+        average:5.4,
+    },
+    {
+        name: 'Jakub Kawecki',
+        attendance: '55%',
+        average:3.5,
+    },
+    {
+        name: 'Krzysztof K bo rodo',
+        attendance: '25%',
+        average:1.5,
+    },
+
+]
