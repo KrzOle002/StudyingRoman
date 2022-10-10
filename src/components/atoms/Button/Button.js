bf36748cb921dc25ee721c113f4421aa51b68fb3
Xkriso <thekriso@wp.pl>
2022-10-10 19:08:18 +0200

    Add additional folders for styles

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,23 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import { StyledButton } from "./Button.style";
 import { ReactComponent as DeleteIcon } from 'assets/icon/delete-icon.svg';
-
-const StyledButton = styled.button`
-    width: 25px;
-    height: 25px;
-    background-color: #c8c7d6;
-    border-radius: 50px; 
-    border: none;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    svg{
-        width: 100%;
-        height: 100%;
-    }
-`;
 
 const Button = (props) => {
     return (
--- src/components/organisms/UserList/UserList.js
+++ src/components/organisms/UserList/UserList.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import { users } from 'data/users';
 import UserListItem from 'components/molecules/UserListItem/UserListItem';
-import styled from 'styled-components';
-
-const Wraper = styled.div`
-    background-color: white;
-    width: 100%;
-    max-width: 500px;
-    padding: 40px 30px;
-    border-radius: 25px;
-    box-shadow: 0 5px 15px -10px rgba(0,0,0,0.3);
-`;
-
-const StyledList = styled.ul`
-    list-style: none;
-    margin: 0;
-    padding: 0;
-`;
+import { StyledList, Wraper } from './UserList.style'
 
 const UserList = () => {
 
